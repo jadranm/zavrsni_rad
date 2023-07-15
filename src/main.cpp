ff6b6255349dae3a6206e96a55766aa8bce3dc7b
Jadran Marinovic <jmarinovi@tvz.hr>
2023-07-15 08:29:39 +0200

    pocetak koda

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
-#include <Arduino.h>
+#include<Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_INTEGRIRANI 2
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+	pinMode(LED_INTEGRIRANI, OUTPUT);
+
+	//startup sekvenca
+	delay(1000);
+	
+	for (uint8_t i = 0; i < 3; i++){
+		digitalWrite(LED_INTEGRIRANI, HIGH);
+		delay(500);
+		digitalWrite(LED_INTEGRIRANI, LOW);
+		delay(500);
+	}
+	
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+	
 }
